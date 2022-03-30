ce8c32d9429298166d6130ad0db980b516abd5d9
Олександр <95880849+Alexandr-Olegovich@users.noreply.github.com>
2022-03-31 01:05:51 +0300

    final
    final

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,15 @@
 (() => {
-  const refs = {
+  const expo = {
     openMenuBtn: document.querySelector(".data-menu-open"),
     closeMenuBtn: document.querySelector(".data-menu-close"),
     Menu: document.querySelector(".data-menu"),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  expo.openMenuBtn.addEventListener("click", toggleMenu);
+  expo.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-    refs.Menu.classList.toggle("is-hidden");
+    expo.Menu.classList.toggle("is-hidden");
   }
 })();
 
